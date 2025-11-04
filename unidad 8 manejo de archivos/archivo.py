5902104f044cdc6488afbe3775268f219ecfad8b
matydominguez87 <matydominguez87@gmail.com>
2025-11-04 20:29:56 -0300

    Delete unidad 8 manejo de archivos/archivo.py

--- unidad 8 manejo de archivos/archivo.py
+++
@@ -1,8 +0,0 @@
-## 1 CREAR ARCHIVO ##
-print("1. Escribiendo archivo")
-archivo = open("archivo.txt", "w")
-archivo.write("bicicleta ,200.000, 23\n")
-archivo.write("moto ,5.400.000, 12\n")
-archivo.write("auto ,15.000.000, 5\n")
-archivo.close()
-print("Archivo creado\n")
